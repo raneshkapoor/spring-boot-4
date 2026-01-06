b257fa3bdb9899a29d6cd9b5b07bc2faed1389ee
ranesh <raneshkapoor@ymail.com>
2026-01-06 20:23:28 +0530

    added module dev tools actuator

---
+++ spring-boot-dev-tools-actuator/src/main/java/org/springpractice/devtoolsactuator/SpringBootDevToolsActuatorApplication.java
@@ -0,0 +1,13 @@
+package org.springpractice.devtoolsactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevToolsActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDevToolsActuatorApplication.class, args);
+    }
+
+}
---
+++ spring-boot-dev-tools-actuator/src/test/java/org/springpractice/devtoolsactuator/SpringBootDevToolsActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springpractice.devtoolsactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDevToolsActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
