0b6a8445510d8019c72ba2ff44aca6e8e247a690
ranesh <raneshkapoor@ymail.com>
2026-01-06 17:47:06 +0530

    Added Spring Boot Project

---
+++ src/test/java/org/springboot4/SpringBoot4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboot4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
