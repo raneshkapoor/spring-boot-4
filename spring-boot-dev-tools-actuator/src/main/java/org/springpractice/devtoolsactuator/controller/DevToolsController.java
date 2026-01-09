b802cd8416c76010f0585d1a1d5b4ab4c1ac4840
ranesh <raneshkapoor@ymail.com>
2026-01-09 21:21:01 +0530

    actuator changes

--- spring-boot-dev-tools-actuator/src/main/java/org/springpractice/devtoolsactuator/controller/DevToolsController.java
+++ spring-boot-dev-tools-actuator/src/main/java/org/springpractice/devtoolsactuator/controller/DevToolsController.java
@@ -1,9 +1,11 @@
 package org.springpractice.devtoolsactuator.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/devtools")
 public class DevToolsController {
 
     @GetMapping("/testapi")
