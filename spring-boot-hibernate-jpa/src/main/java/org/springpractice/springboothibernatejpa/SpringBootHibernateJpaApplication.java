3b84e23d2afe9b1b84c08f1f5da6f9fc0fe511ee
ranesh <raneshkapoor@ymail.com>
2026-03-20 16:08:54 +0530

    multi module build, added jpa module

---
+++ spring-boot-hibernate-jpa/src/main/java/org/springpractice/springboothibernatejpa/SpringBootHibernateJpaApplication.java
@@ -0,0 +1,13 @@
+package org.springpractice.springboothibernatejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHibernateJpaApplication.class, args);
+    }
+
+}
---
+++ spring-boot-hibernate-jpa/src/test/java/org/springpractice/springboothibernatejpa/SpringBootHibernateJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springpractice.springboothibernatejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHibernateJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
