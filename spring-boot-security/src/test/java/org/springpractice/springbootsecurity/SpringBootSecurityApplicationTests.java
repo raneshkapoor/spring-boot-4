d202c767ff43b54f7f0c06e36e585654ae7d3cfa
ranesh <raneshkapoor@ymail.com>
2026-03-31 01:39:55 +0530

    added module spring-security

---
+++ spring-boot-security/src/test/java/org/springpractice/springbootsecurity/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springpractice.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
