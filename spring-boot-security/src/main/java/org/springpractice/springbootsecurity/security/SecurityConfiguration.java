c1f3e1a37d56ca830d49504d7d900e55a632a987
ranesh <raneshkapoor@ymail.com>
2026-04-10 03:16:47 +0530

    added role based authentication with db

--- spring-boot-security/src/main/java/org/springpractice/springbootsecurity/security/SecurityConfiguration.java
+++ spring-boot-security/src/main/java/org/springpractice/springbootsecurity/security/SecurityConfiguration.java
@@ -6,15 +6,16 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
 
-    @Bean
+    /*@Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
 
         UserDetails john = User.builder()
@@ -36,6 +37,11 @@ public class SecurityConfiguration {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }*/
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
